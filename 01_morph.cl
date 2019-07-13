83180218a7741b49e252f7f2c7e25c72b79d46ef
fluxrider <doompilot@gmail.com>
2019-07-13 10:57:40 -0400

    dilate (from web examples)

---
+++ 01_morph.cl
@@ -0,0 +1,20 @@
+# This code is derived from:
+# https://towardsdatascience.com/get-started-with-gpu-image-processing-15e34b787480
+
+__constant sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_FILTER_NEAREST | CLK_ADDRESS_CLAMP_TO_EDGE;
+
+__kernel void dilate(__read_only image2d_t in, __write_only image2d_t out) {
+  const int x = get_global_id(0);
+  const int y = get_global_id(1);
+
+  // find max in 3x3 region
+  float extreme = 0;
+  for(int i = -1; i <= 1; i++) {
+    for(int j = -1; j <= 1; j++) {
+      const float pixel = read_imagef(in, sampler, (int2)(x + i, y + j)).s0;
+      extreme = max(extreme, pixel);
+    }
+  }
+
+  write_imagef(out, (int2)(x, y), (float4)(extreme, 0.0f, 0.0f, 0.0f));
+}
---
+++ 01_morph.py
@@ -0,0 +1,39 @@
+# This code is a mixture of:
+# http://www.drdobbs.com/open-source/easy-opencl-with-python/240162614
+# https://towardsdatascience.com/get-started-with-gpu-image-processing-15e34b787480
+
+import cv2
+import numpy as np
+import pyopencl as cl
+
+# read image from file (the 'host' is the cpu)
+in_host = cv2.imread('in.png', cv2.IMREAD_GRAYSCALE)
+shape = in_host.T.shape
+out_host = np.empty_like(in_host)
+
+# setup OpenCL
+platforms = cl.get_platforms()
+platform = platforms[0]
+devices = platform.get_devices()
+device = devices[0]
+context = cl.Context([device])
+queue = cl.CommandQueue(context, device)
+
+# create image buffers which hold images for OpenCL (the 'device' is the gpu)
+grayscale_format = cl.ImageFormat(cl.channel_order.LUMINANCE, cl.channel_type.UNORM_INT8)
+in_device = cl.Image(context, cl.mem_flags.READ_ONLY, grayscale_format, shape=shape)
+out_device = cl.Image(context, cl.mem_flags.WRITE_ONLY, grayscale_format, shape=shape)
+
+# load, compile OpenCL program, and fetch function ptr
+program = cl.Program(context, open('01_morph.cl').read()).build()
+dilate = cl.Kernel(program, 'dilate')
+
+# copy image to device, execute kernel, copy data back
+cl.enqueue_copy(queue, in_device, in_host, origin=(0, 0), region=shape, is_blocking=False)
+dilate.set_arg(0, in_device)
+dilate.set_arg(1, out_device)
+cl.enqueue_nd_range_kernel(queue, dilate, shape, None)
+cl.enqueue_copy(queue, out_host, out_device, origin=(0, 0), region=shape, is_blocking=True)
+
+# write output to file
+cv2.imwrite('out.png', out_host)
