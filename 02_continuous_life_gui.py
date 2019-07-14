99f6c21b84b8bc036117637813f0f4b84bf01d9e
fluxrider <doompilot@gmail.com>
2019-07-14 12:53:20 -0400

    (ongoing) conway gui

---
+++ 02_continuous_life_gui.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import PIL.Image
+import PIL.ImageTk
+root = tk.Tk()
+
+image_pil = PIL.Image.open('conway_init.png')
+image_tk = PIL.ImageTk.PhotoImage(image_pil)
+W, H = image_pil.size
+
+canvas = tk.Canvas(root, width=W, height=H)
+canvas_image = canvas.create_image(0, 0, anchor=tk.NW, image=image_tk)
+canvas.pack()
+
+def refresh_image(canvas, canvas_image, i):
+  global image_tk
+  image_pil = PIL.Image.open(f'out{i}.png')
+  print(image_pil)
+  image_tk = PIL.ImageTk.PhotoImage(image_pil)
+  canvas.itemconfig(canvas_image, image=image_tk)
+  canvas.after(10, refresh_image, canvas, canvas_image, i + 1)
+refresh_image(canvas, canvas_image, 0)
+root.mainloop()
