935a73f7bb26a4d5cc01f4a530226a927052fa77
fluxrider <doompilot@gmail.com>
2019-07-14 12:05:12 -0400

    generation loop

--- 02_continuous_life.py
+++ 02_continuous_life.py
@@ -19,21 +19,25 @@ format_f32 = cl.ImageFormat(cl.channel_order.LUMINANCE, cl.channel_type.FLOAT)
 
 # read image from file as grayscale uint8 and send it to the gpu
 image = np.asarray(PIL.Image.open('conway_init.png').convert('L'))
-image_gpu = cl.Image(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, format_u8, shape=image.shape, hostbuf=image)
 
-# isotropic histogram filter
-map = np.empty(image.shape, dtype='float32')
-map_gpu = cl.Image(context, cl.mem_flags.WRITE_ONLY, format_f32, shape=image.shape)
-program.map_cdf(queue, image.shape, None, map_gpu, image_gpu)
-cl.enqueue_copy(queue, map, map_gpu, origin=(0, 0), region=image.shape, is_blocking=True)
-smooth = gaussian_filter(map, sigma=2.0)
+for j in range(100):
+  image_gpu = cl.Image(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, format_u8, shape=image.shape, hostbuf=image)
 
-# run game of life
-out_gpu = cl.Image(context, cl.mem_flags.WRITE_ONLY, format_u8, shape=image.shape)
-smooth_gpu = cl.Image(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, format_f32, shape=image.shape, hostbuf=smooth.astype('float32'))
-program.continuous_life(queue, image.shape, None, out_gpu, image_gpu, smooth_gpu)
-out = np.empty_like(image)
-cl.enqueue_copy(queue, out, out_gpu, origin=(0, 0), region=image.shape, is_blocking=True)
+  # isotropic histogram filter
+  map_gpu = cl.Image(context, cl.mem_flags.WRITE_ONLY, format_f32, shape=image.shape)
+  program.map_cdf(queue, image.shape, None, map_gpu, image_gpu)
+  map = np.empty(image.shape, dtype='float32')
+  cl.enqueue_copy(queue, map, map_gpu, origin=(0, 0), region=image.shape, is_blocking=True)
+  smooth = gaussian_filter(map, sigma=2.0)
 
-# save
-PIL.Image.fromarray(out).save('out.png')
+  # run game of life
+  out_gpu = cl.Image(context, cl.mem_flags.WRITE_ONLY, format_u8, shape=image.shape)
+  smooth_gpu = cl.Image(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, format_f32, shape=image.shape, hostbuf=smooth.astype('float32'))
+  program.continuous_life(queue, image.shape, None, out_gpu, image_gpu, smooth_gpu)
+  out = np.empty_like(image)
+  cl.enqueue_copy(queue, out, out_gpu, origin=(0, 0), region=image.shape, is_blocking=True)
+
+  # save
+  PIL.Image.fromarray(out).save(f'out{j}.png')
+  
+  image = out
