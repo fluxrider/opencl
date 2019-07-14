6f4e379fd6766845029890c584659e169023ce91
fluxrider <doompilot@gmail.com>
2019-07-14 12:55:23 -0400

    (ongoing) conway gui

--- 02_continuous_life_gui.py
+++ 02_continuous_life_gui.py
@@ -4,8 +4,10 @@ import PIL.ImageTk
 root = tk.Tk()
 
 image_pil = PIL.Image.open('conway_init.png')
-image_tk = PIL.ImageTk.PhotoImage(image_pil)
 W, H = image_pil.size
+
+# tk remark: the variable passed as image in the canvas must be kept around
+image_tk = None
 
 canvas = tk.Canvas(root, width=W, height=H)
 canvas_image = canvas.create_image(0, 0, anchor=tk.NW, image=image_tk)
@@ -14,7 +16,6 @@ canvas.pack()
 def refresh_image(canvas, canvas_image, i):
   global image_tk
   image_pil = PIL.Image.open(f'out{i}.png')
-  print(image_pil)
   image_tk = PIL.ImageTk.PhotoImage(image_pil)
   canvas.itemconfig(canvas_image, image=image_tk)
   canvas.after(10, refresh_image, canvas, canvas_image, i + 1)
