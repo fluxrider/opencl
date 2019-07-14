ac50eb31e1d0f7624d06d6ac2c46c8fdfa3d48b4
fluxrider <doompilot@gmail.com>
2019-07-13 20:51:17 -0400

    continuous game of life (cpu) (slow)

--- 02_isotropic_histogram.py
+++ 02_isotropic_histogram.py
@@ -1,21 +1,8 @@
-# port: GaussianI1
-# from scipy.stats import norm
-# norm(loc=mean, scale=stddev).cdf(a)
-
-# port: CubicInterpolator
-# from scipy.interpolate import interp1d
-# f = interp1d(x, y, kind='cubic')
-# f(a)
-
-# port: RecursiveGaussianFilter
-# from scipy.ndimage import gaussian_filter
-# m2 = gaussian_filter(m, sigma=2)
-
 # A variant of Conway Game of Life which uses continuous values.
 # It uses an Isotropic Histogram Filter to gather information about the neighborhood. Filter is based on:
 # Smoothed Local Histogram Filters Pixar Technical Memo 10-02 by Michael Kass and Justin Solomon
 
-import cv2
+import PIL.Image # pip install Pillow
 import numpy as np
 import pyopencl as cl
 from scipy.stats import norm
@@ -34,14 +21,13 @@ samples = 20
 sigmaK = 13.0
 sigmaW = 2.0
 
-# read image from file
-image = cv2.imread('02.png', cv2.IMREAD_COLOR)
-shape = image.T.shape
+# read image from file as grayscale
+image = np.asarray(PIL.Image.open('conway_init_small.png').convert('L'))
 
 # execute
-W = shape[0]
-H = shape[1]
-out = np.empty_like(image)
+H = image.shape[0]
+W = image.shape[1]
+out = np.zeros(image.shape)
 
 # isotropic histogram filter
 # compute values for s[i]
@@ -52,6 +38,7 @@ map = np.zeros((samples, W, H))
 smooth = np.empty((samples, W, H))
 
 for i in range(samples):
+  print(i)
   # compute lookup table for each intensities
   gauss = norm(loc=0, scale=sigmaK)
   for intensity in range(depth):
@@ -60,8 +47,7 @@ for i in range(samples):
   for y in range(H):
     for x in range(W):
       # scale pixel intensity to depth
-      r8, g8, b8 = image[x, y]
-      intensity = max(0, min(1, (0.2125 * r8 + 0.7154 * g8 + 0.0721 * b8) / 255)) * (depth - 1)
+      intensity = int((image[y, x] / 255) * (depth - 1))
       # map with lookup
       map[i, x, y] = lookup[i, intensity]
 
@@ -77,9 +63,8 @@ for i in range(samples):
 # for each pixel
 for y in range(H):
   for x in range(W):
-    r8, g8, b8 = image[x, y]
-    # convert rgb to life force (gray_dx_normalized)
-    life = max(0, min(1, (0.2125 * r8 + 0.7154 * g8 + 0.0721 * b8) / 255))
+    # convert pixel to life force
+    life = image[y, x] / 255
     # get the percentage of life in the neighborhood
     aliveNeighborhood = interpolation[x][y](aliveThreshold * 255)
 
@@ -106,7 +91,7 @@ for y in range(H):
       else: life = 1 - (abs(aliveNeighborhood - half) / delta)
     
     # convert life to rgb
-    out[x, y] = [life * 255] * 3
-    print(out[x, y])
+    out[y, x] = max(0, min(255, life * 255))
+    print((x,y))
     
-cv2.imwrite('out.png', out)
+PIL.Image.fromarray(np.uint8(out)).save('out.png')
